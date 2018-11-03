ec9f65e3afd6f20d72bba73672084d851b7865cc
Maham Shahid <mahum.shahid934@gmail.com>
2018-11-04 04:29:20 +0500

    Detail view functionality added

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ImageService } from '../../services/image.service';
+
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  imgName: string;
+  imgSrc: string;
+  imgCreatedDate: string;
+
+  constructor(private imgServ: ImageService) {
+    this.imgName = null;
+    this.imgSrc = null;
+    this.imgCreatedDate = null;
+  }
 
   ngOnInit() {
+    this.imgName = this.imgServ.currentClickedName;
+    this.imgSrc = this.imgServ.getImage(this.imgName)[0];
+    this.imgCreatedDate = this.imgServ.getImage(this.imgName)[1];
   }
 
 }
