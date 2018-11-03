6b46549f2185f074e71ef236516f112a112abc80
Maham Shahid <mahum.shahid934@gmail.com>
2018-11-04 04:27:53 +0500

    Helper service added in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ImageService } from './services/image.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private service: ImageService) {  }
 }
