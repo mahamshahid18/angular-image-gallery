f612df30605e6c422a33eae95d25145a4d0e2916
Maham Shahid <mahum.shahid934@gmail.com>
2018-11-04 03:27:26 +0500

    Main gallery view component created: view + add images functionality complete

---
+++ src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  images = null;
+  imgKeys = null;
+
+  ngOnInit() {
+    this.updateImageUI();
+  }
+
+  addImage(event) {
+    let fileData = null;
+    let fileName = null;
+    let fileCreatedAtDate = null;
+
+    if (event.target.files && event.target.files[0]) {
+      fileName = event.target.files[0].name;
+      fileCreatedAtDate = new Date();
+
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (data: any) => { // called once readAsDataURL is completed
+        fileData = data.target.result;
+        this.storeImage(fileName, fileCreatedAtDate, fileData);
+      };
+    }
+  }
+
+  storeImage(name, date, data) {
+    let storedImages = this.getStoredImages();
+
+    if (storedImages === null || storedImages === undefined) {
+      storedImages = {};
+    }
+
+    storedImages[name] = [data, date];
+    localStorage.setItem('images', JSON.stringify(storedImages));
+
+    this.updateImageUI();
+  }
+
+  getStoredImages() {
+    return JSON.parse(localStorage.getItem('images'));
+  }
+
+  updateImageUI() {
+    this.images = this.getStoredImages();
+    this.imgKeys = Object.keys(this.images);
+  }
+
+}
