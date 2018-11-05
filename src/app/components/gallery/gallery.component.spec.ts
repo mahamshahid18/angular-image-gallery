713a0204b7a59fc957d3f73276f670c0425dc765
Maham Shahid <mahum.shahid934@gmail.com>
2018-11-06 02:25:21 +0500

    Unit tests added for Gallery Component

--- src/app/components/gallery/gallery.component.spec.ts
+++ src/app/components/gallery/gallery.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GalleryComponent } from './gallery.component';
+import { ImageService } from 'src/app/services/image.service';
 
 describe('GalleryComponent', () => {
   let component: GalleryComponent;
-  let fixture: ComponentFixture<GalleryComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
-    })
-    .compileComponents();
-  }));
+  let service: ImageService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GalleryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = new ImageService();
+    component = new GalleryComponent(service);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should expect numCols property to be populated', () => {
+    expect(component.numCols).not.toBeUndefined();
+  });
+
+  it('should expect imgClick function to call service', () => {
+    const spy = spyOn(service, 'imgClicked');
+
+    component.imgClick('', 0);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should expect getImgAddedDateString function to return a string', () => {
+    const addedDateString = component.getImgAddedDateString('11-11-11');
+
+    expect(addedDateString).not.toBeUndefined();
+    expect(typeof(addedDateString)).toBe('string');
   });
 });
